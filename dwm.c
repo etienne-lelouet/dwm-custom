6ab011bb1b4771f9b26827dc0ad9677058023d06
Etienne Le Louet <etiennelelouet@outlook.com>
2020-04-19 12:32:58 +0200

    Revert "fix for compatibility between single tagset and noborder"
    This reverts commit 5c83c984f15f498193588741b688593946a99fb6.

--- dwm.c
+++ dwm.c
@@ -1387,7 +1387,7 @@ resizeclient(Client *c, int x, int y, int w, int h)
 	c->oldw = c->w; c->w = wc.width = w;
 	c->oldh = c->h; c->h = wc.height = h;
 	wc.border_width = c->bw;
-	if (((nexttiled(c->mon->cl->clients, c->mon) == c && !nexttiled(c->next, c->mon))
+	if (((nexttiled(c->mon->clients) == c && !nexttiled(c->next))
 	    || &monocle == c->mon->lt[c->mon->sellt]->arrange)
 	    && !c->isfullscreen && !c->isfloating) {
 		c->w = wc.width += c->bw * 2;
